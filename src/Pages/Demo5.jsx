543ec23a3d48b1157f12d7c1dae17fc7ee64afe4
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-04-25 19:33:13 +0545

    Some changes

--- src/Pages/Demo5.jsx
+++ src/Pages/Demo5.jsx
@@ -1,108 +1,124 @@
-import React from 'react'
+import React from "react";
 
 const Demo5 = () => {
-    const Card=[
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        },
-        {
-            id: 1,
-            image: "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            title: "How to keep Going when you Don't know what's Next",
-            description: "The future can be scary,but there are ways to deal with that fear.",
-            avatar: "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            name: "Jerome Walton",
-            date: "Yesterday"
-        }
-
-
-
-    ]
-    console.log(Card);
+  const Card = [
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+    {
+      id: 1,
+      image:
+        "https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      title: "How to keep Going when you Don't know what's Next",
+      description:
+        "The future can be scary,but there are ways to deal with that fear.",
+      avatar:
+        "https://plus.unsplash.com/premium_photo-1747375615029-515941ee7a99?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      name: "Jerome Walton",
+      date: "Yesterday",
+    },
+  ];
+  console.log(Card);
   return (
-    <div className='container3'>
-        {Card.map((card , index)=>(
-            <div className='card' key={index}>
-                <img src={card.image} alt="Logo"></img>
-                <div className='content'>
-                    <div className='title'>
-                        {card.title}
-                    </div>
-                    <div className ='description'>
-                        {card.description}
-                    </div>
-                    <div className='avatar'>
-                        <img src={card.avatar} alt="Avatar"></img>
-                        <div className='avatar-info'>
-                            <div className='name'>{card.name}</div>
-                            <div className='date'>{card.date}</div>
-                        </div>
-                    </div>
-                </div>
+    <div className="container3">
+      {Card.map((card, index) => (
+        <div className="card" key={index}>
+          <img src={card.image} alt="Logo"></img>
+          <div className="content">
+            <div className="title">{card.title}</div>
+            <div className="description">{card.description}</div>
+            <div className="avatar">
+              <img src={card.avatar} alt="Avatar"></img>
+              <div className="avatar-info">
+                <div className="name">{card.name}</div>
+                <div className="date">{card.date}</div>
+              </div>
             </div>
-
-        ))}
-        {/* <div className='card'>
+          </div>
+        </div>
+      ))}
+      {/* <div className='card'>
             <img src="https://images.unsplash.com/photo-1747586181200-96551a018ed1?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D "
             alt="Logo"></img>
             <div className='content'>
@@ -127,11 +143,8 @@ const Demo5 = () => {
             </div>
 
         </div> */}
-
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Demo5
+export default Demo5;
