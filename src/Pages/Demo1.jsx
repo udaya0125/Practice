c0c3f483b7162bfe5c55bc5a98ca8fcce52e4300
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-04-25 19:31:47 +0545

    Some changes

--- src/Pages/Demo1.jsx
+++ src/Pages/Demo1.jsx
@@ -1,45 +1,53 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 const Demo1 = () => {
-    const [searchTerm , setSearchTerm ]= useState('')
-    const [location , setLocation]= useState('')
+  const [searchTerm, setSearchTerm] = useState("");
+  const [location, setLocation] = useState("");
 
+  const [formData, setFormData] = useState({
+    searchTerm: "",
+    location: "",
+  });
 
-    const [formData , setFormData] = useState({
-        searchTerm: '',
-        location: ''
-    })
+  const handleChange = (e) => {
+    console.log(e);
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
 
-
-    const handleChange = (e) => {
-        console.log(e)
-        const { name,value} = e.target
-        setFormData({
-            ...formData,
-            [name]:value
-        })
-
-    }
-
-    console.log(formData) 
+  console.log(formData);
   return (
-    <div className='search'> 
-        <div className='search-box'>
-            <input name='searchTerm' value={formData.searchTerm} onChange={handleChange} type="text" placeholder='Search Keyword' 
-            
-            />    
-            <label for='location'></label>
-            <select value={formData.location}  name='location' onChange={handleChange} id='location'>
-                <option value="Online Jobs in Bergen">Online Jobs in Bergen</option>
-                <option value="Online Jobs in Stanvanger">Online Jobs in Stavanger</option>
-                <option value="locOnline Jobs in Tronsa">Online Jobs in Tronsa</option> 
-            </select>
-                    
-            <button className='btn' >Search</button>
-        </div>
-    
-    </div>
-  )
-}
+    <div className="search">
+      <div className="search-box">
+        <input
+          name="searchTerm"
+          value={formData.searchTerm}
+          onChange={handleChange}
+          type="text"
+          placeholder="Search Keyword"
+        />
+        <label for="location"></label>
+        <select
+          value={formData.location}
+          name="location"
+          onChange={handleChange}
+          id="location">
+          <option value="Online Jobs in Bergen">Online Jobs in Bergen</option>
+          <option value="Online Jobs in Stanvanger">
+            Online Jobs in Stavanger
+          </option>
+          <option value="locOnline Jobs in Tronsa">
+            Online Jobs in Tronsa
+          </option>
+        </select>
 
-export default Demo1
+        <button className="btn">Search</button>
+      </div>
+    </div>
+  );
+};
+
+export default Demo1;
