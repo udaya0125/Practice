4b309e07ff93c238d26495834ca6098f37cc51fe
Udaya Adhikari <adhikariudaya521@gmail.com>
2026-04-25 19:32:19 +0545

    Some changes

--- src/Pages/Demo3.jsx
+++ src/Pages/Demo3.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Contain from '../components/Contain'
-import Card from '../components/Card'
+import React from "react";
+import Navbar from "../components/Navbar";
+import Contain from "../components/Contain";
+import Card from "../components/Card";
 const Demo3 = () => {
   return (
-    <div className='bodyone'>
-
-      <Navbar/>
-        <Contain/>
-        <Card/>
-      
+    <div className="bodyone">
+      <Navbar />
+      <Contain />
+      <Card />
     </div>
-  )
-}
+  );
+};
 
-export default Demo3
+export default Demo3;
